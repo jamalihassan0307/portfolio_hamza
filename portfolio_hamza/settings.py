4c14b67bc3e06eb13cd4d90cf092bea5b6e9b5f9
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-06-04 22:40:41 +0500

    Enhance security settings in settings.py: define ALLOWED_HOSTS for production and implement security measures for non-debug mode, including SSL redirection and HSTS configurations.

--- portfolio_hamza/settings.py
+++ portfolio_hamza/settings.py
@@ -26,7 +26,26 @@ SECRET_KEY = 'django-insecure-2tozv0*lt$4h=x-vr#eanlv7!rsjb9by6_%lhd7fta6mn4p&1z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'hamzahasan5124.pythonanywhere.com',
+    'www.hamzahasan5124.pythonanywhere.com',
+    'localhost',
+    '127.0.0.1',
+]
+
+# Security settings for production
+if not DEBUG:
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_BROWSER_XSS_FILTER = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    X_FRAME_OPTIONS = 'DENY'
+    SECURE_HSTS_SECONDS = 31536000  # 1 year
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+
+
 
 
 # Application definition
